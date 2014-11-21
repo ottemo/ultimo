657f86f5543861802bcf5f774f33c97f1bc02720
Viktor Silvestrov <vsilvestrov@speroteck.com>
2014-11-21 03:06:54 -0800

    bug fixing

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 require.iniConfig = {
-    "general.app.foundation_url": "http://localhost:3000",
+    "general.app.foundation_url": "http://dev.ottemo.io:3000",
     "general.app.media_path": "media/",
     "themes.list.active": "default",
     // social networks
