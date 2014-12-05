48e0308b71c7615455caf12e57ab9eea5c1b9cb4
zheyakush <zhenya1708@inbox.ru>
2014-12-05 14:13:15 +0200

    A small fixes in js

--- app/themes/blitz/scripts/pdp/controller.js
+++ app/themes/blitz/scripts/pdp/controller.js
@@ -3,54 +3,55 @@
 
     define(["pdp/init"], function (pdpModule) {
 
-        pdpModule
-        /**
-         *  HTML top page header manipulator (direct service mapping)
-         */
-            .controller("pdpControllerBlitz", [
-                "$scope",
-                "$controller",
-                "$routeParams",
-                "$location",
-                "$pdpApiService",
-                "$pdpProductService",
-                "$designImageService",
-                "$cartService",
-                "$visitorLoginService",
-                // TODO: reduce the number of statements in the function below and remove jshint comment
-                function ($scope,$controller, $routeParams, $location, $pdpApiService, $pdpProductService, $designImageService, $cartService, $visitorLoginService) {
-                    $controller('pdpController', {$scope: $scope});
+            pdpModule
+            /**
+             *  HTML top page header manipulator (direct service mapping)
+             */
+                .controller("pdpControllerBlitz", [
+                    "$scope",
+                    "$controller",
+                    "$routeParams",
+                    "$location",
+                    "$timeout",
+                    "$pdpApiService",
+                    "$pdpProductService",
+                    "$designImageService",
+                    "$cartService",
+                    "$visitorLoginService",
+                    // TODO: reduce the number of statements in the function below and remove jshint comment
+                    function ($scope, $controller, $routeParams, $location, $timeout, $pdpApiService, $pdpProductService, $designImageService, $cartService, $visitorLoginService) {
+                        $controller('pdpController', {$scope: $scope});
 
-                    $scope.addToCart = function () {
-                        $visitorLoginService.isLoggedIn().then(function (isLoggedIn) {
-                            if (isLoggedIn) {
-                                $scope.submitted = true;
-                                $cartService.add($scope.productId, $scope.qty, $pdpProductService.getOptions()).then(
-                                    function (response) {
-                                        if (response.error !== "") {
-                                            $scope.messageOptions = {
-                                                'type': 'danger',
-                                                'message': response.error
-                                            };
-                                        } else {
-                                            var miniCart;
-                                            miniCart = $("#mini-cart");
-                                            miniCart.addClass('active')
-                                            setTimeout(function () {
-                                                miniCart.removeClass('active');
-                                            }, 2000);
+                        $scope.addToCart = function () {
+                            $visitorLoginService.isLoggedIn().then(function (isLoggedIn) {
+                                if (isLoggedIn) {
+                                    $scope.submitted = true;
+                                    $cartService.add($scope.productId, $scope.qty, $pdpProductService.getOptions()).then(
+                                        function (response) {
+                                            if (response.error !== "") {
+                                                $scope.messageOptions = {
+                                                    'type': 'danger',
+                                                    'message': response.error
+                                                };
+                                            } else {
+                                                var miniCart;
+                                                miniCart = $("#mini-cart");
+                                                miniCart.addClass('active');
+                                                $timeout(function () {
+                                                    miniCart.removeClass('active');
+                                                }, 2000);
+                                            }
                                         }
-                                    }
-                                );
-                            } else {
-                                $("#form-login").modal("show");
-                            }
-                        });
-                    };
- }
-            ]);
+                                    );
+                                } else {
+                                    $("#form-login").modal("show");
+                                }
+                            });
+                        };
+                    }
+                ]);
 
             return pdpModule;
         }
     );
-})(window.define,jQuery);
+})(window.define, jQuery);
