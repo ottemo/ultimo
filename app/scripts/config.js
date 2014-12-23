89b20a0ed10da50582e28417c872050a75f314a9
zheyakush <zhenya1708@inbox.ru>
2014-12-23 19:15:03 +0200

    Fixed config file

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,10 +1,10 @@
 require.iniConfig = {
-    "general.app.foundation_url": "http://localhost:3000",
+    "general.app.foundation_url": "http://dev.ottemo.io:3000",
     "general.app.media_path": "media/",
     "themes.list.active": "blitz",
     // social networks
-    "general.app.login.facebook.appId" : "692415160866472",
-    "general.app.login.facebook.secretKey" : "4548dbe2f3d96876ef9d6aab97fec4f8",
+    "general.app.login.facebook.appId" : "728744980537129",
+    "general.app.login.facebook.secretKey" : "58f763bd74fab043aa3b3c0221291c94",
     "general.app.login.google.clientId" : "1074763412644-qq25glj3tb87bq7bk5m8793da11ddheh.apps.googleusercontent.com",
 
     // general
