a20f96750e4ca049df195c96cf8f627da8b04c5c
James Vastbinder <james.vastbinder@gmail.com>
2015-01-19 10:11:42 -0800

    update vagrant usage
    [#86440796]

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -1,9 +1,20 @@
-apt-get update
-apt-get install -y nginx git curl python-software-properties python software-properties-common
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs
+#!/bin/bash
+
+# This is an install script for Ubuntu, run as a user with sudo priveleges
+
+# install nginx, vagrant, virtualbox and node.js
+sudo apt-get update
+sudo apt-get install -y nginx git curl python-software-properties python software-properties-common
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs vagrant virtualbox virtualbox-dkms
+
+# update npm
 npm update -g npm
+
+# set up ottemo store and nginx
+# TODO: add setup for vagrant and nginx
+sudo mkdir -p /vagrant
 cd /vagrant
 npm install
 npm install -g bower
