44fd65ac01224266d38442cec78e3ac789966cdf
Adam Knox <adam.w.knox@gmail.com>
2015-05-29 10:46:51 -0400

    [#95521582] add the check success sidebar
    * also made the order id a link to the account order section

--- app/scripts/checkout/init.js
+++ app/scripts/checkout/init.js
@@ -3,53 +3,49 @@
  */
 angular.module("checkoutModule", ["ngRoute", "ngResource", "designModule"])
 
-    .constant("CHECKOUT_TYPE", "general.checkout.checkout_type")
-    .constant("ONEPAGE_URL", "/spcheckout")
-    .constant("ACCORDION_URL", "/checkout")
+.constant("CHECKOUT_TYPE", "general.checkout.checkout_type")
+.constant("ONEPAGE_URL", "/spcheckout")
+.constant("ACCORDION_URL", "/checkout")
 
-    /*
-     *  Basic routing configuration
-     */
-    .config([
-        "$routeProvider",
-        "$locationProvider",
-        "ONEPAGE_URL",
-        "ACCORDION_URL",
-        function ($routeProvider, $locationProvider, ONEPAGE_URL, ACCORDION_URL) {
-            $routeProvider
-                .when(ONEPAGE_URL, {
-                    templateUrl: "theme/views/checkout/view.html",
-                    controller: "checkoutOnepageController"
-                })
-                .when(ACCORDION_URL, {
-                    templateUrl: "theme/views/checkout/view2.html",
-                    controller: "checkoutAccordionController"
-                })
-                .when('/checkout/success/:orderId', {
-                    templateUrl: "theme/views/checkout/order-confirmation.html",
-                    controller: "checkoutOrderConfirmationController"
-                })
-                .when('/checkout/success', {
-                    templateUrl: "theme/views/checkout/order-confirmation.html",
-                    controller: "checkoutOrderConfirmationController"
-                });
-            $locationProvider.html5Mode(true);
-        }
-    ])
-
-    .run([
-        "$http",
-        "REST_SERVER_URI",
-        "CHECKOUT_TYPE",
-        "$checkoutService",
-        function ($http, REST_SERVER_URI, CHECKOUT_TYPE, $checkoutService) {
-            $http({
-                url: REST_SERVER_URI + "/config/value/" + CHECKOUT_TYPE,
-                method: "GET"
-            }).success(function (response) {
-                $checkoutService.setType(response.result);
+.config([
+    "$routeProvider",
+    "$locationProvider",
+    "ONEPAGE_URL",
+    "ACCORDION_URL",
+    function ($routeProvider, $locationProvider, ONEPAGE_URL, ACCORDION_URL) {
+        $routeProvider
+            .when(ONEPAGE_URL, {
+                templateUrl: "theme/views/checkout/view.html",
+                controller: "checkoutOnepageController"
+            })
+            .when(ACCORDION_URL, {
+                templateUrl: "theme/views/checkout/view2.html",
+                controller: "checkoutAccordionController"
+            })
+            .when('/checkout/success/:orderId', {
+                templateUrl: "theme/views/checkout/order-confirmation.html",
+                controller: "checkoutOrderConfirmationController"
+            })
+            .when('/checkout/success', {
+                templateUrl: "theme/views/checkout/order-confirmation.html",
+                controller: "checkoutOrderConfirmationController"
             });
-        }
-    ]
-);
+        $locationProvider.html5Mode(true);
+    }
+])
+
+.run([
+    "$http",
+    "REST_SERVER_URI",
+    "CHECKOUT_TYPE",
+    "$checkoutService",
+    function ($http, REST_SERVER_URI, CHECKOUT_TYPE, $checkoutService) {
+        $http({
+            url: REST_SERVER_URI + "/config/value/" + CHECKOUT_TYPE,
+            method: "GET"
+        }).success(function (response) {
+            $checkoutService.setType(response.result);
+        });
+    }
+]);
 
