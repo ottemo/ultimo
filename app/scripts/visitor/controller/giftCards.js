49733a9324858bf3085be7c984b21e73a2e78bea
Adam Knox <adam.w.knox@gmail.com>
2015-06-08 13:53:58 -0400

    [#96316468] Display the gift card amount a little better and handle empty input

--- app/scripts/visitor/controller/giftCards.js
+++ app/scripts/visitor/controller/giftCards.js
@@ -16,15 +16,20 @@ angular.module("visitorModule")
 		$scope.checkBalance = function() {
 			$scope.searching = true;
 
-			$visitorApiService.getGiftCardBalance({"giftcode": $scope.giftcode}).$promise
-			.then(function(response) {
-				$scope.searching = false;
-				$scope.error = false;
-				if (response.error && response.error.code == "dd7b2130-b5ed-4b26-b1fc-2d36c3bf147f") {
-					$scope.error = "Invalid giftcard code";
-				}
-				$scope.giftcardDetails = response.result || false;
-			});
+			if ($scope.giftcode) {
+				$visitorApiService.getGiftCardBalance({"giftcode": $scope.giftcode}).$promise
+				.then(function(response) {
+					$scope.searching = false;
+					$scope.error = false;
+					if (response.error && response.error.code == "dd7b2130-b5ed-4b26-b1fc-2d36c3bf147f") {
+						$scope.error = "Invalid giftcard code";
+					}
+					$scope.giftcardDetails = response.result || false;
+				});
+			} else {
+				$scope.error = "Please enter a gift card code";
+
+			}
 		}
 	}
 ]);
