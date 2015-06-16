330f99506f1f898416af03bb205097bc065e46fb
vanischenko <vanischenko@speroteck.com>
2015-06-16 16:27:30 +0300

    [#92728736] Password Requirements Display

--- app/scripts/design/directives/validator/password.js
+++ app/scripts/design/directives/validator/password.js
@@ -70,4 +70,71 @@ angular.module("designModule")
                 ngModel.$formatters.unshift(validate);
             }
         };
+    })
+
+    // New Password Directive
+    .directive("otPassword2", function () {
+        return {
+            restrict: 'EA',
+            require: '?ngModel',
+            link: function (scope, elem, attrs, ngModel) {
+
+                var minLen = 8,
+                maxLen = 18,
+                minCountUppercase = 1,
+                minCountLowercase = 1,
+                minCountNumbers = 1,
+                minCountSymbols = 1;
+
+                var checkLowercases = function (value) {
+                    var matches = value.match(/([a-z]+)/g);
+                    return (matches === null || (matches !== null && matches.join("").length < minCountLowercase));
+                };
+                var checkUppercases = function (value) {
+                    var matches = value.match(/([A-Z]+)/g);
+                    return (matches === null || (matches !== null && matches.join("").length < minCountUppercase));
+                };
+                var checkNumbers = function (value) {
+                    var matches = value.match(/([\d]+)/g);
+                    return (matches === null || (matches !== null && matches.join("").length < minCountNumbers));
+                };
+                var checkSymbols = function (value) {
+                    var matches = value.match(/([\!\@\#\\$\%\^\&\*\(\)\_\+\-\~]+)/g);
+                    return (matches === null || (matches !== null && matches.join("").length < minCountSymbols));
+                };
+
+                var validate = function (value) {                    /*jshint maxcomplexity:7 */
+                    if (!value) { return value; }
+                    ngModel.invalids = [];
+                    var valid = true;
+                    if (!(value.length >= minLen && value.length < maxLen)) {
+                        valid = false;
+                        ngModel.invalids['length'] = true;
+                    }
+                    if (checkLowercases(value)) {
+                        valid = false;
+                        ngModel.invalids['lowercase'] = true;
+                    }
+                    if (checkUppercases(value)) {
+                        valid = false;
+                        ngModel.invalids['uppercase'] = true;
+                    }
+                    if (checkNumbers(value)) {
+                        valid = false;
+                        ngModel.invalids['number'] = true;
+                    }
+                    if (checkSymbols(value)) {
+                        valid = false;
+                        ngModel.invalids['symbol'] = true;
+                    }
+                    ngModel.$setValidity('ot-password2', valid);
+                    return value;
+                };
+
+                //For DOM -> model validation
+                ngModel.$parsers.unshift(validate);
+                //For model -> DOM validation
+                ngModel.$formatters.unshift(validate);
+            }
+        };
     });
