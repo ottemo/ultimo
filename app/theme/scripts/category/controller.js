665226d3eeb69ab87af1dc1e02f8e27b5ca83b5b
Adam Knox <adam.w.knox@gmail.com>
2015-06-26 11:30:26 -0400

    hero image updates
    * [#97849524] lip treatment
    * [#97901696] natural lip balm hero

--- app/theme/scripts/category/controller.js
+++ app/theme/scripts/category/controller.js
@@ -357,9 +357,8 @@ angular.module('categoryModule')
                     '5512f1cdd4a2560a14000082': '/theme/images/category/lip-treatment.jpg',
                     '5512f248d4a2560a14000094': '/theme/images/category/makeup.jpg',
                     '5548fc2188a87b4350000197': '/theme/images/category/Kari-Gran-Shop-Page-Gifts-Organic-Skincare-Desktop.jpg',
-
-                    // Lip Color
-                    '5575e50588a87b4255000015': '/theme/images/category/lip-color.jpg',
+                    '5575e50588a87b4255000015': '/theme/images/category/lip-color.jpg',                 // Lip Color
+                    '5575e4f188a87b4255000013': '/theme/images/category/lip-treatment_06-26-2015.jpg',  // Lip Treatment
                 };
 
                 if ($scope.categoryId !== null) {
