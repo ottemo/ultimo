f09a5a3e09a92f6511f2110149c4cf85b6d8669c
vanischenko <vanischenko@speroteck.com>
2015-06-26 16:13:25 +0300

    [#92728736] new password part2

--- app/scripts/design/directives/validator/password.js
+++ app/scripts/design/directives/validator/password.js
@@ -1,83 +1,19 @@
 angular.module("designModule")
 
-    .directive("otPassword", function () {
-        return {
-            restrict: 'EA',
-            require: '?ngModel',
-            link: function (scope, elem, attrs, ngModel) {
-
-                var minLen, minCountUppercase, minCountLowercase, minCountNumbers, minCountSymbols, passwordNotValidLength,
-                passwordNotEnoughLowercases, passwordNotEnoughUppercases, passwordNotEnoughNumbers, passwordNotEnoughSymbols;
-
-                minLen = 8;
-                minCountUppercase = 1;
-                minCountLowercase = 1;
-                minCountNumbers = 1;
-                minCountSymbols = 1;
-
-                passwordNotValidLength = "password should have " + minLen + " char or more";
-                passwordNotEnoughLowercases = "password should have at least " + minCountUppercase + " lowercase";
-                passwordNotEnoughUppercases = "password should have at least " + minCountUppercase + " uppercase";
-                passwordNotEnoughNumbers = "password should have at least " + minCountNumbers + " numbers";
-                passwordNotEnoughSymbols = "password should have at least " + minCountSymbols + " symbols";
-
-                var checkLowercases = function (value) {
-                    var matches = value.match(/([a-z]+)/g);
-                    return (matches === null || (matches !== null && matches.join("").length < minCountLowercase));
-                };
-                var checkUppercases = function (value) {
-                    var matches = value.match(/([A-Z]+)/g);
-                    return (matches === null || (matches !== null && matches.join("").length < minCountUppercase));
-                };
-                var checkNumbers = function (value) {
-                    var matches = value.match(/([\d]+)/g);
-                    return (matches === null || (matches !== null && matches.join("").length < minCountNumbers));
-                };
-                var checkSymbols = function (value) {
-                    var matches = value.match(/([\!\@\#\\$\%\^\&\*\(\)\_\+\-\~]+)/g);
-                    return (matches === null || (matches !== null && matches.join("").length < minCountSymbols));
-                };
-                var validate = function (value) {                    /*jshint maxcomplexity:7 */
-                    if (!value) { return value; }
-                    var valid = true;
-                    if (value.length < minLen) {
-                        valid = false;
-                        ngModel.message = passwordNotValidLength;
-                    }
-                    if (checkLowercases(value)) {
-                        valid = false;
-                        ngModel.message = passwordNotEnoughLowercases;
-                    }
-                    if (checkUppercases(value)) {
-                        valid = false;
-                        ngModel.message = passwordNotEnoughUppercases;
-                    }
-                    if (checkNumbers(value)) {
-                        valid = false;
-                        ngModel.message = passwordNotEnoughNumbers;
-                    }
-                    if (checkSymbols(value)) {
-                        valid = false;
-                        ngModel.message = passwordNotEnoughSymbols;
-                    }
-                    ngModel.$setValidity('ot-password', valid);
-                    return value;
-                };
-
-                //For DOM -> model validation
-                ngModel.$parsers.unshift(validate);
-                //For model -> DOM validation
-                ngModel.$formatters.unshift(validate);
-            }
-        };
-    })
-
     // New Password Directive
     .directive("otPassword2", function () {
         return {
             restrict: 'EA',
             require: '?ngModel',
             link: function (scope, elem, attrs, ngModel) {
+
+                ngModel.rules = {
+                    "length": "Your password may be any combination of 8 to 18 characters.",
+                    "uppercase": "It must contain at least 1 uppercase letter.",
+                    "lowercase": "It must contain at least 1 lowercase letter.",
+                    "number": "It must contain at least 1 number.",
+                    "symbol": "It must contain at least 1 of following special characters: (!, @, #, $, &, *)."
+                };
 
                 var minLen = 8,
                 maxLen = 18,
