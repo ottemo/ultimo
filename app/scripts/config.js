f499816bf49daa25b71597c7065a6833e285ffe2
James Vastbinder <james.vastbinder@gmail.com>
2015-07-21 11:56:27 -0700

    add in local dev config commented out
    [#99317942]

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -4,6 +4,10 @@ var iniConfig = {
     "general.app.foundation_url": "http://foundation.kg.dev.ottemo.io",
     "general.app.media_path": "//kg.dev.ottemo.io/media/",
 
+    // for local development w/foundation
+    //"general.app.foundation_url": "http://localhost:3000",
+    //"general.app.media_path": "/kg-media/",
+
     // social networks
     "general.app.login.facebook.appId" : "553891724751037",
     "general.app.login.facebook.secretKey" : "1b0aed24d291d155cccaf115fc976184",
