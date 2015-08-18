c5478a307389e5f71d63ad63bff6d592438210dd
James Vastbinder <james.vastbinder@gmail.com>
2015-08-18 16:03:09 -0700

    no shippable builds and build during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# SRCDIR
+SRCDIR=/home/ottemo/storefront
+MEDIADIR=/home/ottemo/media
+
 if [ "$BRANCH" == 'develop' ]; then
-    scp -r dist ottemo@$REMOTE_HOST:~/storefront/
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git pull"
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && rm -rf media"
+    # build locally after successful merge to develop
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && NODE_ENV=production gulp build && cd dist && ln -s $MEDIADIR media"
+    # restart nginx
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && sudo /etc/init.d/nginx restart"
 fi
