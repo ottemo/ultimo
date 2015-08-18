595255297115a57021402bb8b7cfbd5e7a69e099
James Vastbinder <james.vastbinder@gmail.com>
2015-08-18 16:24:37 -0700

    do remote builds

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ MEDIADIR=/home/ottemo/media
 
 if [ "$BRANCH" == 'develop' ]; then
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git pull"
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && rm -rf media"
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && rm -rf media && npm install -g gulp&& npm install"
     # build locally after successful merge to develop
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && NODE_ENV=production gulp build && cd dist && ln -s $MEDIADIR media"
     # restart nginx
