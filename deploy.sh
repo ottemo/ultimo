8f9e69662ffb5210f9b2131a34d61cd8d8c7804f
James Vastbinder <james.vastbinder@gmail.com>
2015-08-18 19:58:47 -0700

    run gulp build in dev mode on feature branches

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,25 @@ SRCDIR=/home/ottemo/storefront
 MEDIADIR=/home/ottemo/media
 
 if [ "$BRANCH" == 'develop' ]; then
-		echo "Updating remote git reposistory with develop branch."
+		echo "\nUpdating remote git reposistory with develop branch."
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git pull"
-    echo "Removing symlink to media folder and installing gulp and npm."
+    echo "\nRemoving symlink to media folder and installing gulp and npm."
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && rm -rf media && npm install -g gulp && npm install"
     # build locally after successful merge to develop
-    echo "Running gulp build and restoring symlink to media folder."
+    echo "\nRunning gulp build and restoring symlink to media folder."
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && NODE_ENV=production gulp build && cd dist && ln -s $MEDIADIR media"
     # restart nginx
-    echo "Restarting nginx."
+    echo "\nRestarting nginx."
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && sudo /etc/init.d/nginx restart"
+  elif [[ "$BRANCH" != "develop" ]]; then
+		echo "\nUpdating remote git reposistory with ${BRANCH} branch."
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git checkout -f ${BRANCH} && git pull -u origin ${BRANCH}"
+    echo "\nRemoving symlink to media folder and installing gulp and npm."
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && rm -rf media && npm install -g gulp && npm install"
+    # build locally after successful merge to develop
+    echo "\nRunning gulp build and restoring symlink to media folder."
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && gulp build && cd dist && ln -s $MEDIADIR media"
+    # restore develop branch on dev server
+    echo "\nRestoring git repository on dev server."
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git checkout -f develop"
 fi
