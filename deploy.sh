ed28718736a6045defbfce86827bf0bb0e236720
Adam Willoughby-Knox <adam.w.knox@gmail.com>
2015-08-24 09:49:02 -0400

    clean up quotes and spaces

--- deploy.sh
+++ deploy.sh
@@ -5,31 +5,29 @@ SRCDIR=/home/ottemo/storefront
 MEDIADIR=/home/ottemo/media
 
 if [ "$BRANCH" == 'develop' ]; then
-    echo ""
-    echo "UPDATING REMOTE GIT REPOSITORY WITH DEVELOP BRANCH."
-    echo ""
+    echo 
+    echo UPDATING REMOTE GIT REPOSITORY WITH DEVELOP BRANCH.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git pull"
-    echo ""
-    echo "REMOVING DIST DIRECTORY."
-    echo ""
+    
+    echo 
+    echo REMOVING DIST DIRECTORY.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && rm -rf dist"
-    echo ""
-    echo "INSTALL GULP AND RUN NPM INSTALL"
-    echo ""
+    
+    echo 
+    echo INSTALL GULP AND RUN NPM INSTALL
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && npm install -g gulp && npm install"
-    # build locally after successful merge to develop
-    echo ""
-    echo "RUNNING PRODUCTION GULP BUILD AND RESTORING SYMLINK TO MEDIA FOLDER."
-    echo ""
+    
+    echo 
+    echo RUNNING PRODUCTION GULP BUILD AND RESTORING SYMLINK TO MEDIA FOLDER.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && NODE_ENV=production gulp build"
-    echo ""
-    echo "RESTORING DIST DIRECTORY."
-    echo ""
+    
+    echo
+    echo RESTORING DIST DIRECTORY.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && ln -s $MEDIADIR media"
-    # restart nginx
-    echo ""
-    echo "RESTARTING NGINX."
-    echo ""
+    
+    
+    echo 
+    echo RESTARTING NGINX.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && sudo /etc/init.d/nginx restart"
 else
     # echo ""
