e525e29ab2d6ac3954289a013980ba982a996182
Adam Knox <adam.w.knox@gmail.com>
2015-09-15 15:33:59 -0400

    remove unused directive that we solved with a polyfill, and add a comment

--- app/scripts/design/directives/design.js
+++ app/scripts/design/directives/design.js
@@ -1,21 +1,4 @@
 angular.module("designModule")
-    /*
-     *  Directive to solve browser auto-fill issue on model
-     */
-    .directive("autoFillSync", ["$timeout", function ($timeout) {
-        return {
-            require: "ngModel",
-            link: function (scope, elem, attrs, ngModel) {
-                var origVal = elem.val();
-                $timeout(function () {
-                    var newVal = elem.val();
-                    if (ngModel.$pristine && origVal !== newVal) {
-                        ngModel.$setViewValue(newVal);
-                    }
-                }, 500);
-            }
-        };
-    }])
 
     /*
      *  jQuery layout directive
