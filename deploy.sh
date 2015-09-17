7992b125f0ac0a281f97e37e5fdfd383d9e73eaa
James Vastbinder <james.vastbinder@gmail.com>
2015-09-17 22:40:26 +0000

    update deploys for develop branch on staging
    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 # SRCDIR
-SRCDIR=/home/ottemo/storefront
-MEDIADIR=/home/ottemo/media
+HOME=/home/ottemo
+SRCDIR=$HOME/code/karigran
+MEDIADIR=$HOME/media
 
 if [ "$BRANCH" == 'develop' ]; then
     echo ""
     echo UPDATING REMOTE GIT REPOSITORY WITH DEVELOP BRANCH.
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git fetch --prune && git pull"
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && git checkout develop && git fetch --prune && git pull"
 
     echo ""
     echo REMOVING DIST DIRECTORY.
     ssh ottemo@$REMOTE_HOST "cd $SRCDIR && rm -rf dist"
 
     echo ""
-    echo INSTALL GULP AND RUN NPM INSTALL
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && npm install -g gulp && npm install"
-
-    echo ""
     echo RUNNING PRODUCTION GULP BUILD AND RESTORING SYMLINK TO MEDIA FOLDER.
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && NODE_ENV=production gulp build"
+    ssh ottemo@$REMOTE_HOST "cd $SRCDIR && gulp build"
 
     echo ""
     echo RESTORING DIST DIRECTORY.
-    ssh ottemo@$REMOTE_HOST "cd $SRCDIR/dist && ln -s $MEDIADIR media"
-
+    ssh ottemo@$REMOTE_HOST "rm -rf $HOME/store/* && cp -R $SRCDIR/dist/* $HOME/store && ln -s $MEDIADIR $HOME/store/media"
 
     echo ""
-    echo RESTARTING NGINX.
+    echo DEPLOY FINISHED
 fi
