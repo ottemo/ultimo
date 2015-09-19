5dab29d4a4ef0876cff5177cfb411848d4988fab
James Vastbinder <james.vastbinder@gmail.com>
2015-09-18 22:43:41 -0700

    update deploy process to support all environments
    [#103016316]

---
+++ config/config.js
@@ -0,0 +1,11 @@
+var iniConfig = {
+    'general.app.foundation_url': '@@apiUrl',
+
+    // social networks
+    'general.app.login.facebook.appId' : '@@idFacebook',
+    'general.app.login.google.clientId' : '@@idGoogle',
+
+    // general
+    'general.app.category.itemsPerPage' : @@itemsPerPage,
+    'general.checkout.guest_checkout' : @@guestCheckout,
+};
