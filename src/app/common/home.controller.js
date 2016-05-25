d170723c2f74d7caf3fcc15adf8e83f3d01cb2f1
Dimitry <dglushenkov@speroteck.com>
2016-05-25 22:08:46 +0300

    CMS based homepage slider (#69) [#119375281]

--- src/app/common/home.controller.js
+++ src/app/common/home.controller.js
@@ -5,16 +5,6 @@
 
     function HomeController($scope) {
 
-        $scope.sliderContent = [{
-            img: '/images/common/home/slide1.jpg',
-            link: '/mens'
-        }, {
-            img: '/images/common/home/slide2.jpg',
-            link: '/womens'
-        }, {
-            img: '/images/common/home/slide3.jpg',
-            link: '/sale'
-        }];
     }
 
 })();
